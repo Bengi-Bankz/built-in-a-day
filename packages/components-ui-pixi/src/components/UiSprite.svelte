18b824a0fa9e3891cec4baa1a31b05bdcef18b64
Bengimen Harkins <https://github.com/Bengi-Bankz>
2025-08-16 03:34:52 -0700

    Initial commit

---
+++ packages/components-ui-pixi/src/components/UiSprite.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" module>
+	import { Rectangle, type RectangleProps } from 'pixi-svelte';
+
+	export type Props = RectangleProps;
+</script>
+
+<script lang="ts">
+	const props: Props = $props();
+</script>
+
+<Rectangle
+	borderRadius={50}
+	backgroundColor={0xadd8e6}
+	backgroundAlpha={0.75}
+	borderColor={0x000000}
+	borderWidth={4}
+	{...props}
+/>
